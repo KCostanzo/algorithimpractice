94b6b7e37e8665c9424dc3e90f6dc64a545f9df1
KCostanzo <kstanz22@gmail.com>
2017-09-22 11:20:32 -0700

    str permutations

--- Probs/chi.js
+++ Probs/chi.js
@@ -26,9 +26,62 @@ const checkStringUniqueChars = str => {
 }
 
 
-checkStringUniqueChars("abcdefghijk");
-checkStringUniqueChars("abcdedc");
-checkStringUniqueChars("lool");
-checkStringUniqueChars("jimlpotreae");
-console.log("n2, not great");
+// checkStringUniqueChars("abcdefghijk");
+// checkStringUniqueChars("abcdedc");
+// checkStringUniqueChars("lool");
+// checkStringUniqueChars("jimlpotreae");
+// console.log("n2, not great");
 console.log("2: \n");
+
+
+const chceckStrPermutation = (str1, str2) => {
+
+	var checkObj = {};
+
+	if (str1.length != str2.length) {
+		console.log("false, diff length \n");
+		return false;
+	}
+
+	for (var i = 0; i < str1.length; i++) {
+		if (typeof checkObj[str1[i]] == "number") {
+			// console.log("repeat char");
+			checkObj[str1[i]] += 1;
+		} else {
+			checkObj[str1[i]] = 1;
+		}
+		console.log(checkObj);
+	}
+
+	for (var j=0; j < str1.length; j++) {
+		if (typeof checkObj[str2[j]] == 'number') {
+			checkObj[str2[j]] -= 1;
+			console.log("str2 char chceck");
+			console.log(checkObj);
+		} else {
+			console.log("false, not matching char");
+			return false;
+		}
+	}
+
+	for (var key in checkObj) {
+		if (checkObj[key] !== 0) {
+			console.log("mismatched char numbers");
+			console.log(key + checkObj[key]);
+			return false;
+		}
+	}
+
+
+	console.log("true");
+	return true;
+}
+
+
+
+chceckStrPermutation("abcd", "abdc"); //true
+chceckStrPermutation("abcdef", "zetyol"); //false
+chceckStrPermutation("checking", "check"); //false
+chceckStrPermutation("longcheck", "gonechlck"); //true
+chceckStrPermutation("abcbcba", "cbaaccb"); //false, last case
+
